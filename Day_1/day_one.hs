1d9ac88cf97c33c1a1f242d5aba0fa64e112a049
eleocraft <eliseo.cailloux@gmx.de>
2025-12-03 03:12:04 +0100

    Day 1 star 2
    Really inelegant solution :(

--- Day_1/day_one.hs
+++ Day_1/day_one.hs
@@ -1,16 +1,27 @@
 import System.IO
+import Debug.Trace
 
 main = do
-        contents <- readFile "test.txt"
-        print . getNumZeros 50 . map readInfo . lines $ contents
+        contents <- readFile "input.txt"
+        print . getNumZeros 50 . map readMove . lines $ contents
 
-readInfo :: String -> (Char, Int)
-readInfo (h:t) = (h, read t)
+readMove :: String -> Int
+readMove (h:t) = if h == 'L' then negate num else num 
+    where num = read t
 
-getNumZeros :: Int -> [(Char, Int)] -> Int
+getNumZeros :: Int -> [Int] -> Int
 getNumZeros _ [] = 0
-getNumZeros x ((dir, n):rest) = ifZero newPos + getNumZeros newPos rest
-        where newPos = case dir of
-                            'L' -> mod (x - n) 100
-                            'R' -> mod (x + n) 100
-              ifZero y = if y == 0 then 1 else 0
+getNumZeros x (m:rest) = countZero x m + getNumZeros newPos rest
+        where newPos = mod (x + m) 100
+              countZero x m
+                  | m < 0 = isMod (x - 1) + countZero (mod (x - 1) 100) (m + 1)
+                  | m > 0 = isMod (x + 1) + countZero (mod (x + 1) 100) (m - 1)
+                  | otherwise = 0
+              isMod x = if mod x 100 == 0 then 1 else 0
+
+-- first part of exersise
+-- getNumZeros :: Int -> [Int] -> Int
+-- getNumZeros _ [] = 0
+-- getNumZeros x (n:rest) = ifZero newPos + getNumZeros newPos rest
+--         where newPos = mod (x + n) 100
+--               ifZero y = if y == 0 then 1 else 0
